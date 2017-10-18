9593934608efb3e7b0c50ce4aff659a3c2bff85d
Cristofer Gonzalez Cruz <dark.wolfrain@gmail.com>
2017-10-18 12:44:30 -0300

    identity

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  public identity = false;
+  public identity = true;
 
   constructor() { }
 
