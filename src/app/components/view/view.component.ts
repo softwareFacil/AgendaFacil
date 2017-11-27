49cb339fdeacfdf9de26274194e2bab6e293ce98
Cristofer Gonzalez Cruz <dark.wolfrain@gmail.com>
2017-11-26 21:15:56 -0300

    cristofer

--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -23,27 +23,27 @@ export class ViewComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      console.log(params);
       this.evento = params;
-      console.log(this.evento);
-      this.geocodeAddres(this.evento.ubicacion);
+      this.geocode(this.evento);
     });
   }
 
   ngAfterViewInit() {
     this.map = new google.maps.Map(document.getElementById('map'), {
       zoom: 14,
-      center: {lat: -34.397, lng: 150.644}
     });
   }
 
-  geocodeAddres(address){
-    this.geocoder.geocode({'address':address},(results, status)=>{
+  geocode(latlong){
+    var input = latlong.lat+','+latlong.long;
+    var latlngStr = input.split(',', 2);
+    var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+    this.geocoder.geocode({'location':latlng},(results, status)=>{
       if (status.toString() === 'OK') {
         this.map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
           map: this.map,
-          position: results[0].geometry.location
+          position: latlng
         });
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
